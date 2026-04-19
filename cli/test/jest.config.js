e0bd34b4fd9c171116961489e2cf6c32056c975f
Syrotenko Igor <Neloreck@gmail.com>
2026-04-19 20:46:47 +0300

    Add coverage output.

--- cli/test/jest.config.js
+++ cli/test/jest.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  coveragePathIgnorePatterns: ["/node_modules/", "/fixtures/", "/types/", "/__tests__/"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/fixtures/", "/types/", "/examples/", "/target/", "/__tests__/"],
+  collectCoverage: true,
   coverageDirectory: "<rootDir>/target/coverage",
   setupFilesAfterEnv: [path.resolve(__dirname, "setup_tests.js")],
   rootDir: "../..",
