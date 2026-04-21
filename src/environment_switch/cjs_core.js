bc9af2001b85e26d2c95d6533f68fb630bfaa8ab
Syrotenko Igor <Neloreck@gmail.com>
2026-04-21 20:07:23 +0300

    Remove preparation script env switchers.

--- src/environment_switch/cjs_core.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./cjs/production/index.js");
-} else {
-  module.exports = require("./cjs/development/index.js");
-}
--- src/environment_switch/cjs_utils.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./cjs/production/test-utils.js");
-} else {
-  module.exports = require("./cjs/development/test-utils.js");
-}
