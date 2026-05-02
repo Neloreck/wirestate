123ad3235fb0f7e134cfcaaa34bed0e86029241d
Neloreck <Neloreck@gmail.com>
2026-05-02 16:07:14 +0300

    Separate features check mobx dir.

---
+++ examples/features-check-mobx/src/libs/wirestate/signals.js
@@ -0,0 +1,15 @@
+"use no memo";
+export {
+  Signal,
+  action,
+  batch,
+  computed,
+  createModel,
+  effect,
+  signal,
+  untracked,
+  useComputed,
+  useModel,
+  useSignal,
+  useSignalEffect,
+} from "@preact/signals-react"; //# sourceMappingURL=signals.js.map
